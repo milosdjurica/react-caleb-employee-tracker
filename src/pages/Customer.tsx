c34102f0a60580644002a4c49278cd977d07a146
milosdjurica <milosdjurica13@gmail.com>
2023-01-11 11:24:18 +0100

    edit customer done

--- src/pages/Customer.tsx
+++ src/pages/Customer.tsx
@@ -15,6 +15,7 @@ export interface Customer {
 export default function Customer() {
 	const [customer, setCustomer] = useState<Customer>();
 	const [notFound, setNotFound] = useState(false);
+	const [tempCustomer, setTempCustomer] = useState<Customer>();
 
 	const { id } = useParams();
 
@@ -32,44 +33,101 @@ export default function Customer() {
 			})
 			.then((data) => {
 				setCustomer(data);
+				setTempCustomer(data);
 			});
 	}, []);
+
+	function updateCustomer() {
+		const url = baseURL + id;
+		fetch(url, {
+			method: "PATCH",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify(tempCustomer),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				setCustomer(data);
+			})
+			.catch((e) => console.log(e));
+	}
 
 	return (
 		<>
 			{notFound ? (
 				<NotFound message={`Customer with ID '${id}' was not found!`} />
-			) : customer ? (
+			) : tempCustomer ? (
 				<>
 					<h1>Info about customer: </h1>
-					{Object.entries(customer).map(([key, value]) => (
-						<div key={key}>
-							<strong>{key} :</strong> {value}
-						</div>
-					))}
+					<div>
+						<strong>Id : </strong> {tempCustomer._id}
+					</div>
+					<div>
+						<strong>Name : </strong>
+
+						<input
+							className="m-2 px-2"
+							type="text"
+							value={tempCustomer.name}
+							onChange={(e) => {
+								setTempCustomer({ ...tempCustomer, name: e.target.value });
+							}}
+						/>
+					</div>
+					<div>
+						<strong>Industry : </strong>
+						<input
+							className="m-2 px-2"
+							type="text"
+							value={tempCustomer.industry}
+							onChange={(e) => {
+								setTempCustomer({ ...tempCustomer, industry: e.target.value });
+							}}
+						/>
+					</div>
+					<button
+						className="m-2"
+						onClick={() => {
+							const url = baseURL + id;
+							fetch(url, {
+								method: "DELETE",
+								headers: {
+									"Content-type": "application/json",
+								},
+							})
+								.then((res) => {
+									if (!res.ok) {
+										throw new Error("Something went wrong");
+									}
+									alert("Succesfully deleted");
+									navigate("/customers");
+								})
+								.catch((e) => console.log(e));
+						}}
+					>
+						Delete customer
+					</button>
 				</>
 			) : null}
-			<button
-				onClick={(e) => {
-					const url = baseURL + id;
-					fetch(url, {
-						method: "DELETE",
-						headers: {
-							"Content-type": "application/json",
-						},
-					})
-						.then((res) => {
-							if (!res.ok) {
-								throw new Error("Something went wrong");
-							}
-							alert("Succesfully deleted");
-							navigate("/customers");
-						})
-						.catch((e) => console.log(e));
-				}}
-			>
-				Delete
-			</button>
+			{customer?.name !== tempCustomer?.name ||
+			customer?.industry !== tempCustomer?.industry ? (
+				<>
+					<button className="m-2" onClick={updateCustomer}>
+						Save changes
+					</button>
+
+					<button
+						className="m-2"
+						onClick={() => {
+							if (customer) setTempCustomer({ ...customer });
+						}}
+					>
+						Cancel
+					</button>
+				</>
+			) : null}
+
 			<br />
 			<Link to="/customers">Go back</Link>
 		</>
